aa0c78d3cbe4c7b924727c10751d7e1674c12806
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-07-22 15:36:59 -0500

    Solve exercise 3, reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let arr = [];
+  for (let i = 1; i <= string.length; i++) {
+    let nextChar = string[string.length - i];
+    arr.push(nextChar);
+  }
+  const reversedString = arr.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
