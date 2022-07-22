521ac398867fb1cf218b55b262fbdd74534d1552
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-07-22 15:14:09 -0500

    Solve repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } else if (num === 0) {
+    return "";
+  } else {
+    let resultString = "";
+    let i = 0;
+    while (i < num) {
+      resultString = resultString.concat(string);
+      i++;
+    }
+    return resultString;
+  }
 };
 
 // Do not edit below this line
