ea999317c24e66440ce4eba0ce113b691cb7cfe7
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-07-25 12:40:25 -0500

    Solve exercise 7, tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (fTemp) {
+  let celcius = (fTemp - 32) * (5 / 9);
+  celcius = Math.round(celcius * 10) / 10;
+  return celcius;
 };
 
-const ctof = function() {
-
+const ctof = function (cTemp) {
+  let fahrenheit = cTemp * (9 / 5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
