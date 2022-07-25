2e727ea7b19667bee39ac6dd02407c8e95aefb10
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-07-25 11:57:52 -0500

    Solve exercise 5, sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (firstInt, secondInt) {
+  if (firstInt < 0 || secondInt < 0) {
+    return "ERROR";
+  }
+  if (typeof firstInt !== "number" || typeof secondInt !== "number") {
+    return "ERROR";
+  }
+  let smallInt;
+  let largeInt;
+  if (firstInt < secondInt) {
+    smallInt = firstInt;
+    largeInt = secondInt;
+  } else {
+    smallInt = secondInt;
+    largeInt = firstInt;
+  }
+  let sum = 0;
+  while (smallInt <= largeInt) {
+    sum += smallInt;
+    smallInt++;
+  }
+  return sum;
 };
 
 // Do not edit below this line
